8277eb21ea7fdd6095907bfabf6093bf4ca8980f
Federico Guerri <fede95gg@gmail.com>
2018-03-06 18:58:57 +0100

    Added instrumented test for MainActivity.

---
+++ app/src/androidTest/java/com/unifi/federicoguerri/traineeship_android/MainActivityInstrumentedTest.java
@@ -0,0 +1,64 @@
+package com.unifi.federicoguerri.traineeship_android;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.unifi.federicoguerri.traineeship_android", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule  = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void helloWorldViewInMainActivityTest() throws Exception {
+        MainActivity activity = rule.getActivity();
+        View viewById = activity.findViewById(R.id.helloWorldTextView);
+        assertThat(viewById,notNullValue());
+    }
+
+    @Test
+    public void helloWorldViewInMainActivityIsATextViewTest() throws Exception {
+        MainActivity activity = rule.getActivity();
+        View viewById = activity.findViewById(R.id.helloWorldTextView);
+        assertThat(viewById, instanceOf(TextView.class));
+    }
+
+    @Test
+    public void helloWorldTextViewTextIsColorAccentTest() throws Exception {
+        MainActivity activity = rule.getActivity();
+        View viewById = activity.findViewById(R.id.helloWorldTextView);
+        TextView helloworldTextView=(TextView)viewById;
+        int colorAccent=rule.getActivity().getBaseContext().getColor(R.color.colorAccent);
+        assertEquals(helloworldTextView.getCurrentTextColor(), colorAccent);
+    }
+
+}
